ff85d4abdba0d7f14e58110740255709adc80ee0
Zhenya671 <69248073+Zhenya671@users.noreply.github.com>
2021-12-30 18:32:43 +0300

    Update task10.php

--- task10/task10.php
+++ task10/task10.php
@@ -4,27 +4,20 @@ function implementCollatz(int $int)
 {
 
     $int2 = 2;
-    $array = [$int];
+    $array[] = $int;
 
-    if ($int <= 0){
-
+    if ($int <= 0) {
         echo 'the number cannot be less than or equal to 0';
         return false;
-
     }
 
-    while ($int != 1){
+    while ($int != 1) {
 
         if ($int % $int2 == 0) {
-
-            $int = $int/2;
-
+            $int = $int / 2;
         } elseif ($int % $int2 != 0) {
-
             $int = 3 * $int + 1;
-
         }
-
         $array[] = $int;
 
     }

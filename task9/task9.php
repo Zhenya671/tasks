4d4bd6aca0764aa1b838b00b0264b550ffd1ae2c
Zhenya671 <69248073+Zhenya671@users.noreply.github.com>
2021-12-30 18:24:24 +0300

    Update task9.php

--- task9/task9.php
+++ task9/task9.php
@@ -1,21 +1,21 @@
 <?php
 
-function findSumThree(Array $array,int $int): array
+function findSumThree(array $array, int $int): array
 {
 
     $newArr = [];
 
-    for ($i=0;$i <= count($array)-1; $i++) {
+    for ($i = 0; $i <= count($array) - 1; $i++) {
 
         $first = $array[$i];
-        $second = $array[$i+1];
-        $third = $array[$i+2];
-        $sum =  $first + $second + $third;
+        $second = $array[$i + 1];
+        $third = $array[$i + 2];
+        $sum = $first + $second + $third;
 
 
-        if ((!empty($first)) && (!empty($second)) && (!empty($third)) && ($int == $sum)){
+        if ((!empty($first)) && (!empty($second)) && (!empty($third)) && ($int == $sum)) {
 
-                $newArr[] = $int;
+            $newArr[] = $int;
 
         }
 
@@ -29,3 +29,5 @@ $array = [2, 7, 7, 1, 8, 2, 7, 8, 7];
 $int = 16;
 
 print_r(findSumThree($array, $int));
+
+

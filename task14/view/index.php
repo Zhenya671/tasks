3201e662ca9813885760426edd8f5a352f2883fb
Zhenya671 <69248073+Zhenya671@users.noreply.github.com>
2022-01-07 04:19:23 +0300

    Add files via upload

---
+++ task14/view/index.php
@@ -0,0 +1,12 @@
+<form action="//localhost/tests4/task14/model/User.php" method="post">
+
+    email<input id='email' name="email" type="email"  onkeyup='checkParams()'>
+    password<input id='password' name="password" type="password" onkeyup='checkParams()'>
+    <input type='submit' id='submit' value='send' disabled>
+
+</form>
+
+<script src="../task14/other/js/main.js"></script>
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.0.0-rc1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.15.0/jquery.validate.min.js"></script>

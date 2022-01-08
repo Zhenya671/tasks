a66420de2c185533d77c589dd18a61a614330ed5
Zhenya671 <69248073+Zhenya671@users.noreply.github.com>
2022-01-08 09:08:27 +0300

    Update RegisterController.php

--- register/controller/RegisterController.php
+++ register/controller/RegisterController.php
@@ -6,21 +6,13 @@ class registerController extends Controller
     public function index()
     {
         $this->view->render();
-//        var_dump( $this->view);
         if (!empty($_POST)) {
             $action = $_POST['action'];
             if ($action == 'register') {
-//                echo 'hui';
-//                print_r($_POST['signUpForm']);
                 $this->user->signUpUsers($_POST['signUpForm']);
             }
 
-
-
-
         }
-//        $this->view->render();
-
 
     }
 }

7cdd8cec99c9f886f9e9672f694ee428cc1decfd
Zhenya671 <turbanboss193@gmail.com>
2022-01-17 15:34:30 +0300

    completed task18

---
+++ task18/.idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,25 @@
+
+I
+task17final/composer.json,2/e/2ea6f1e79d3dbffa43e660eed56c65c6cd0369f8
+[
++task17final/src/AuthenticationException.php,1/e/1e208491db255f053b0703091b387b6864213080
+L
+task17final/src/Database.php,f/c/fcf1eeb8be9a5ecbfcb4205937fdf9065f561f30
+_
+/task17final/src/FailedAttemptLoginException.php,0/9/09af89d88e6ca251aa132f00fd3c10ae97ad5565
+H
+task17final/src/File.php,1/4/1494207721c71568e16c7ef81fd96f1505089853
+W
+'task17final/src/FileUploadException.php,7/4/7433fa41c4883c759c6c4d4c9356046431deb807
+J
+task17final/src/Logger.php,b/7/b7da2234816a6ba911c633fa2ed88d88b77b080f
+K
+task17final/src/Session.php,a/8/a87dd6d423e1326da1480859d513178484a32e1a
+E
+task17final/index.php,b/a/ba363a1388372613c0adddf52636cdf179563ad9
+R
+"task17final/src/Authentication.php,d/6/d63794bdd51a7aa3626bcdf88856a827466e5675
+J
+task17final/src/Cookie.php,e/1/e1c75ee4e5be3fed42350b3fcb4f841f68e90d1b
+9
+	index.php,b/b/bb6499b8e938f92a3695fff1afe57edea4b9efb7
---
+++ task18/index.php
@@ -0,0 +1,87 @@
+<?php
+const ROOT = __DIR__;
+
+require ROOT . '/task17final/vendor/autoload.php';
+
+class PrintPhpDocs
+{
+
+
+    /**
+     * @throws ReflectionException
+     */
+    public function printDocs()
+    {
+
+        echo '<pre>';
+
+        $potentialClasses = array_diff(scandir(ROOT . '/task17final/src'), ['.', '..']);
+
+        foreach ($potentialClasses as $potentialClass) {
+            $potentialClassName = substr($potentialClass, 0, -4);
+            $class = "App\\" . $potentialClassName;
+
+            $reflection = new ReflectionClass($class);
+            $className = $reflection->getName();
+            $classComment = preg_replace('$\/\*{2}|\*\/$', '', $reflection->getDocComment());
+
+            $classNameContent = "$className ($classComment)<br>";
+            echo $classNameContent;
+
+            $properties = $this->decodeObject($reflection->getProperties());
+            if (!empty($properties)) {
+                echo 'Properties are: <br><br>';
+                $this->getClassInfo($properties, $className, 'Property', $class);
+            }
+
+            $methods = $this->decodeObject($reflection->getMethods());
+            if (!empty($methods)) {
+                echo 'Methods are: <br><br>';
+                $this->getClassInfo($methods, $className, 'Method', $class);
+            }
+
+        }
+    }
+
+    public function decodeObject($object)
+    {
+        $objectEncode = json_encode($object);
+        return json_decode($objectEncode, true);
+    }
+
+    /**
+     * @throws ReflectionException
+     */
+    private function getClassInfo(array $properties, string $className, string $type, string $class)
+    {
+        foreach ($properties as $property) {
+
+            if ($className == $property['class']) {
+
+                switch ($type) {
+
+                    case 'Method':
+                        $propertyData = new ReflectionMethod($class, $property['name']);
+                        break;
+                    case 'Property':
+                        $propertyData = new ReflectionProperty($class, $property['name']);
+                        break;
+                    default:
+                        die('Error');
+                }
+                echo "{$property['name']}";
+                echo '<pre>';
+                echo preg_replace('$\/\*{2}|\*\/$', '', $propertyData->getDocComment());
+                echo '</pre>';
+            }
+        }
+    }
+
+}
+
+$a = new PrintPhpDocs();
+try {
+    $a->printDocs();
+} catch (ReflectionException $e) {
+    throw new UnexpectedValueException('something wrong');
+}

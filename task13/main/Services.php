a8b1008fc2a68390ddd09d5177bedbeb7dc6d825
Zhenya671 <turbanboss193@gmail.com>
2022-01-18 15:21:21 +0300

    add updating task13

--- task13/main/Services.php
+++ task13/main/Services.php
@@ -3,101 +3,27 @@
 abstract class Services
 {
 
-    protected int $costServices = 0;
-    protected array $allCostServices;
-
-    protected string $indent = '<br><br>';
-
-
-    protected function setInfoServices(): array
-    {
-
-        return [
-            'Warranty' => [
-                'deadline' => '31.12.2021',
-                'runQueue' => '',
-                'cost' => 50
-            ],
-            'Delivery' => [
-                'deadline' => '15:00 01.01.2022',
-                'runQueue' => '',
-                'cost' => 15
-            ],
-            'Install' => [
-                'deadline' => '16:00 01.01.2022',
-                'runQueue' => '',
-                'cost' => 150
-            ],
-            'Configure' => [
-                'deadline' => '17:00 01.01.2022',
-                'runQueue' => '',
-                'cost' => 25
-            ],
-        ];
-
-    }
-
-    protected function getInfoWarranty(): User
-    {
-        echo '<br>' . 'Warranty' . $this->indent;
-
-        foreach ($this->setInfoServices()['Warranty'] as $infoAboutService) {
-            echo $infoAboutService . '<br>';
-        }
-
-        $this->allCostServices[] = $this->setInfoServices()['Warranty']['cost'];
-
-        return $this;
-    }
-
-    protected function getInfoDelivery(): User
-    {
-        echo '<br>' . 'Delivery' . $this->indent;
-
-        foreach ($this->setInfoServices()['Delivery'] as $infoAboutService) {
-            echo $infoAboutService . '<br>';
-        }
-
-        $this->allCostServices[] = $this->setInfoServices()['Delivery']['cost'];
-
-        return $this;
-    }
-
-    protected function getInfoConfigure(): User
-    {
-        echo '<br>' . 'Configure' . $this->indent;
-
-        foreach ($this->setInfoServices()['Configure'] as $infoAboutService) {
-            echo $infoAboutService . '<br>';
-        }
-
-        $this->allCostServices[] = $this->setInfoServices()['Configure']['cost'];
-
-        return $this;
-    }
-
-    protected function getInfoInstall(): User
-    {
-        echo '<br>' . 'Install' . $this->indent;
-
-        foreach ($this->setInfoServices()['Install'] as $infoAboutService) {
-            echo $infoAboutService . '<br>';
-        }
-
-        $this->allCostServices[] = $this->setInfoServices()['Install']['cost'];
-
-        return $this;
-    }
-
-    protected function getSumCostServices(): int
-    {
-
-        foreach ($this->allCostServices as $value) {
-            $this->costServices += $value;
-        }
-
-        return $this->costServices;
-
-    }
+    protected array $arrayServices = [
+        'Warranty' => [
+            'deadline' => '31.12.2021',
+            'runQueue' => '',
+            'cost' => 50
+        ],
+        'Delivery' => [
+            'deadline' => '15:00 01.01.2022',
+            'runQueue' => '',
+            'cost' => 15
+        ],
+        'Install' => [
+            'deadline' => '16:00 01.01.2022',
+            'runQueue' => '',
+            'cost' => 150
+        ],
+        'Configure' => [
+            'deadline' => '17:00 01.01.2022',
+            'runQueue' => '',
+            'cost' => 25
+        ],
+    ];
 
 }
--- task13/main/User.php
+++ task13/main/User.php
@@ -5,6 +5,14 @@ class User extends Products implements Methods
 
     protected array $requests;
 
+    protected int $costProducts = 0;
+    protected array $allCostProducts;
+
+    protected int $costServices = 0;
+    protected array $allCostServices;
+
+    protected string $indent = '<br><br>';
+
     public function __construct($request = null, $request2 = null, $request3 = null, $request4 = null)
     {
 
@@ -15,12 +23,143 @@ class User extends Products implements Methods
 
     }
 
+    protected function getInfoTV(): User
+    {
+
+        echo '<br>' . 'TV' . $this->indent;
+
+        foreach ($this->arrayProducts['TV'] as $infoAboutProduct) {
+            echo $infoAboutProduct . '<br>';
+        }
+
+        $this->showServices();
+        $this->allCostProducts[] = $this->arrayProducts['TV']['cost'];
+
+        return $this;
+    }
+
+    protected function getInfoLaptop(): User
+    {
+
+        echo '<br>' . 'Laptop' . $this->indent;
+        foreach ($this->arrayProducts['Laptop'] as $infoAboutProduct) {
+            echo $infoAboutProduct . '<br>';
+        }
+
+        $this->showServices();
+        $this->allCostProducts[] = $this->arrayProducts['Laptop']['cost'];
+
+        return $this;
+    }
+
+    protected function getInfoMobilePhone(): User
+    {
+        echo '<br>' . 'MobilePhone' . $this->indent;
+
+        foreach ($this->arrayProducts['MobilePhone'] as $infoAboutProduct) {
+            echo $infoAboutProduct . '<br>';
+        }
+
+        $this->showServices();
+        $this->allCostProducts[] = $this->arrayProducts['MobilePhone']['cost'];
+
+        return $this;
+    }
+
+    protected function getInfoFridges(): User
+    {
+        echo '<br>' . 'Fridge' . $this->indent;
+
+        foreach ($this->arrayProducts['Fridges'] as $infoAboutProduct) {
+            echo $infoAboutProduct . '<br>';
+        }
+
+        $this->showServices();
+        $this->allCostProducts[] = $this->arrayProducts['Fridges']['cost'];
+
+        return $this;
+    }
+
+    protected function getSumCostProducts(): int
+    {
+
+        foreach ($this->allCostProducts as $value) {
+            $this->costProducts += $value;
+        }
+
+        return $this->costProducts;
+
+    }
+
+    protected function getInfoWarranty(): User
+    {
+        echo '<br>' . 'Warranty' . $this->indent;
+
+        foreach ($this->arrayServices['Warranty'] as $infoAboutService) {
+            echo $infoAboutService . '<br>';
+        }
+
+        $this->allCostServices[] = $this->arrayServices['Warranty']['cost'];
+
+        return $this;
+    }
+
+    protected function getInfoDelivery(): User
+    {
+        echo '<br>' . 'Delivery' . $this->indent;
+
+        foreach ($this->arrayServices['Delivery'] as $infoAboutService) {
+            echo $infoAboutService . '<br>';
+        }
+
+        $this->allCostServices[] = $this->arrayServices['Delivery']['cost'];
+
+        return $this;
+    }
+
+    protected function getInfoConfigure(): User
+    {
+        echo '<br>' . 'Configure' . $this->indent;
+
+        foreach ($this->arrayServices['Configure'] as $infoAboutService) {
+            echo $infoAboutService . '<br>';
+        }
+
+        $this->allCostServices[] = $this->arrayServices['Configure']['cost'];
+
+        return $this;
+    }
+
+    protected function getInfoInstall(): User
+    {
+        echo '<br>' . 'Install' . $this->indent;
+
+        foreach ($this->arrayServices['Install'] as $infoAboutService) {
+            echo $infoAboutService . '<br>';
+        }
+
+        $this->allCostServices[] = $this->arrayServices['Install']['cost'];
+
+        return $this;
+    }
+
+    protected function getSumCostServices(): int
+    {
+
+        foreach ($this->allCostServices as $value) {
+            $this->costServices += $value;
+        }
+
+        return $this->costServices;
+
+    }
+
     public function showProducts(): bool
     {
 
         foreach ($this->requests as $request) {
-            for ($i = 0; $i<= count($this->requests)-1; $i++) {
-                if ($this->requests[$i] == 'TV'|| $this->requests[$i] == 'Laptop'|| $this->requests[$i] == 'mobilePhone'|| $this->requests[$i] == 'Fridges') {
+            for ($i = 0; $i <= count($this->requests) - 1; $i++) {
+                if ($this->requests[$i] == 'TV' || $this->requests[$i] == 'Laptop' || $this->requests[$i] == 'mobilePhone' || $this->requests[$i] == 'Fridges') {
 
                     break;
 
